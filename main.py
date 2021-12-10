2bbcf2ae857dc2e68da4e959f7f3eb6d4cc36a44
Techycicles <93871641+ItzIcicles@users.noreply.github.com>
2021-12-10 21:46:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ screen = tkinter.Entry(root, width=45, borderwidth=5)
 screen.grid(row=0, column=0, columnspan=5, padx=8, pady=10)
 
 
-# Defing functions  
+# Defining functions  
 def button_click(number):
     current = screen.get()
     screen.delete(0, tkinter.END)
@@ -24,22 +24,36 @@ def button_clear():
 def button_add():
     first_number = screen.get()
     global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    screen.delete(0, tkinter.END)
+
+def button_subtract():
+    first_number = screen.get()
+    global f_num
+    global math
+    math = "subtraction"
     f_num = int(first_number)
     screen.delete(0, tkinter.END)
 
 def button_equal():
     second_number = screen.get()
     screen.delete(0, tkinter.END)
-    screen.insert(0, f_num + int(second_number))
+    if math == "addition":
+        screen.insert(0, f_num + int(second_number))
+    if math == "subtraction":
+        screen.insert(0, f_num - int(second_number))
 
 
 # Defining the buttons and placing them on the screen
-button_clear = tkinter.Button(root, text="Clear", padx=125, pady=20, command=button_clear).grid(row=1, column=1, columnspan=3)
+button_clear = tkinter.Button(root, text="Clear", padx=123.99, pady=20, command=button_clear).grid(row=1, column=1, columnspan=3)
 button_add = tkinter.Button(root, text="+", padx=30, pady=20, command=button_add).grid(row=1, column=4)
 
 button_7 = tkinter.Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7)).grid(row=2, column=1)
 button_8 = tkinter.Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8)).grid(row=2, column=2)
 button_9 = tkinter.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9)).grid(row=2, column=3)
+button_subtract = tkinter.Button(root, text="-", padx=31, pady=20, command=button_subtract).grid(row=2, column=4)
 
 button_4 = tkinter.Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4)).grid(row=3, column=1)
 button_5 = tkinter.Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5)).grid(row=3, column=2)
